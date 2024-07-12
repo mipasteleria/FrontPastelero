07216b6a7fa4c16b8953830a287df8158c861761
albores339 <juandediosalboreshernandez@gmail.com>
2024-07-12 02:20:58 -0600

    Navbar

--- front-pastelero/src/components/navbar.jsx
+++ front-pastelero/src/components/navbar.jsx
@@ -72,7 +72,7 @@ const NavbarAdmin = () => {
                 <div className={`${poppins.className} m-4 hidden lg:flex cursor-pointer`}>
                   jalbores339@gmail.com
                 </div>
-                <Link href="/registrarse">
+                <Link href="/enduser/mispedidos">
                   <button className={`${poppins.className} m-4 hidden md:flex cursor-pointer`}>
                     Mis Pedidos
                   </button>
@@ -85,6 +85,27 @@ const NavbarAdmin = () => {
                     ¡Cotizar ahora!
                   </div>
                 </Link>
+                <Link href="/enduser/carrito">
+                  <button className={`${poppins.className} m-4 hidden md:flex cursor-pointer text-text`}>
+                    <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+                    </svg>
+                  </button>
+                </Link>
+                <Link href="/enduser/conocenos#preguntasfrecuentes">
+                  <button className={`${poppins.className} m-4 hidden lg:flex cursor-pointer text-text`}>
+                    <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                    </svg>
+                  </button>
+              </Link>
+              <Link href="/enduser/settings">
+                <button className={`${poppins.className} m-4 cursor-pointer`}>
+                  <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="square" strokeLinejoin="round" strokeWidth="2" d="M10 19H5a1 1 0 0 1-1-1v-1a3 3 0 0 1 3-3h2m10 1a3 3 0 0 1-3 3m3-3a3 3 0 0 0-3-3m3 3h1m-4 3a3 3 0 0 1-3-3m3 3v1m-3-4a3 3 0 0 1 3-3m-3 3h-1m4-3v-1m-2.121 1.879-.707-.707m5.656 5.656-.707-.707m-4.242 0-.707.707m5.656-5.656-.707.707M12 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                  </svg>
+                </button>
+              </Link>
               </div>
             )
           ) : (
@@ -118,6 +139,13 @@ const NavbarAdmin = () => {
                   ¡Cotizar ahora!
                 </div>
               </Link>
+              <Link href="/enduser/conocenos#preguntasfrecuentes">
+                <button className={`${poppins.className} m-4 hidden lg:flex cursor-pointer`}>
+                  <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                  </svg>
+                </button>
+              </Link>
             </div>
           )}
         </div>
@@ -125,7 +153,7 @@ const NavbarAdmin = () => {
       {dropdownOpen && (
         <div
           id="dropdownMenu"
-          className="z-60 fixed left-0 top-0 bottom-0 w-4/5 h-full bg-primary/80 backdrop-blur-sm text-text rounded-lg shadow-2xl p-4 md:hidden"
+          className="z-60 fixed left-0 top-0 bottom-0 w-4/5 h-full bg-primary/80 backdrop-blur-lg text-text rounded-lg shadow-2xl p-4 md:hidden"
         >
           <button className="absolute top-4 right-4" onClick={toggleDropdown}>
             <svg
@@ -147,10 +175,15 @@ const NavbarAdmin = () => {
                 </button>
               </Link>
             ) : (
-              <div>
+              <div className='flex flex-col'>
                 <Link href="/user/orders">
                   <button className={`${poppins.className} m-6 font-bold rounded-xl p-2 text-lg cursor-pointer`}>
                     Mis Pedidos
+                  </button>
+                </Link>
+                <Link href="/user/orders">
+                  <button className={`${poppins.className} m-6 font-bold rounded-xl p-2 text-lg cursor-pointer`}>
+                    LogOut
                   </button>
                 </Link>
                 <Link href="/enduser/solicitarcotizacion">
@@ -158,6 +191,29 @@ const NavbarAdmin = () => {
                     ¡Cotizar ahora!
                   </button>
                 </Link>
+                <div className='flex flex-row text-text mx-6'>
+                  <Link href="/enduser/carrito">
+                    <button className={`${poppins.className} m-4 cursor-pointer`}>
+                      <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+                      </svg>
+                    </button>
+                  </Link>
+                  <Link href="/enduser/conocenos#preguntasfrecuentes">
+                    <button className={`${poppins.className} m-4 cursor-pointer`}>
+                      <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                      </svg>
+                    </button>
+                  </Link>
+                  <Link href="/enduser/settings">
+                    <button className={`${poppins.className} m-4 cursor-pointer`}>
+                    <svg className="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                      <path stroke="currentColor" strokeLinecap="square" strokeLinejoin="round" strokeWidth="2" d="M10 19H5a1 1 0 0 1-1-1v-1a3 3 0 0 1 3-3h2m10 1a3 3 0 0 1-3 3m3-3a3 3 0 0 0-3-3m3 3h1m-4 3a3 3 0 0 1-3-3m3 3v1m-3-4a3 3 0 0 1 3-3m-3 3h-1m4-3v-1m-2.121 1.879-.707-.707m5.656 5.656-.707-.707m-4.242 0-.707.707m5.656-5.656-.707.707M12 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                    </svg>
+                    </button>
+                  </Link>
+                </div>
               </div>
             )
           ) : (
@@ -180,6 +236,11 @@ const NavbarAdmin = () => {
               <Link href="/login">
                 <button className={`${poppins.className} m-6 text-lg cursor-pointer`}>
                   Log In
+                </button>
+              </Link>
+              <Link href="/enduser/conocenos#preguntasfrecuentes">
+                <button className={`${poppins.className} m-6 text-lg cursor-pointer`}>
+                  Preguntas frecuentes
                 </button>
               </Link>
               <Link href="/enduser/solicitarcotizacion">
