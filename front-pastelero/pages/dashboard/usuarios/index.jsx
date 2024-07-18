25e8b4dd098a94858dbc1e6d97ff84f797f2be3c
albores339 <juandediosalboreshernandez@gmail.com>
2024-07-17 22:16:35 -0600

    Tabla pendiente de estilos

--- front-pastelero/pages/dashboard/usuarios/index.jsx
+++ front-pastelero/pages/dashboard/usuarios/index.jsx
@@ -14,7 +14,168 @@ export default function Conocenuestrosproductos() {
         <Asideadmin />
         <main className="flex-grow w-3/4 max-w-screen-lg mx-auto">
         <h1 className={`text-4xl p-4 ${sofia.className}`}>Mis usuarios</h1>
-
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg m-4">
+            <table class="w-full text-sm text-left rtl:text-right">
+                <thead class="text-xs uppercase bg-rose-50">
+                    <tr>
+                        <th scope="col" class="p-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-accent bg-gray-100 border-secondary rounded focus:ring-accent"/>
+                                <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                            </div>
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Nombre
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Role
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Permisos
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="w-4 p-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-accent bg-gray-100 border-gray-300 rounded focus:ring-accent"/>
+                                <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                            </div>
+                        </td>
+                        <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="ps-3">
+                                <div class="text-base font-semibold">Neil Sims</div>
+                                <div class="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                            </div>  
+                        </th>
+                        <td class="px-6 py-4">
+                            Admin
+                        </td>
+                        <td class="px-6 py-4">
+                          <div class="flex flex-col gap-1">
+                                <p>Revisar cotización</p>
+                                <p>Aceptar cotización</p>
+                                <p>Añadir insumos</p>
+                                <p>Acceso al Home</p>
+                                <p>Acceso al Dashboard</p>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar usuario</a>
+                        </td>
+                    </tr>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="w-4 p-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
+                            </div>
+                        </td>
+                        <th scope="row" class="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="ps-3">
+                                <div class="text-base font-semibold">Bonnie Green</div>
+                                <div class="font-normal text-gray-500">bonnie@flowbite.com</div>
+                            </div>
+                        </th>
+                        <td class="px-6 py-4">
+                            End user
+                        </td>
+                        <td class="px-6 py-4">
+                        <div class="flex flex-col gap-1">
+                                <p>solicitar cotización</p>
+                                <p>Acceso al Home</p>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar usuario</a>
+                        </td>
+                    </tr>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="w-4 p-4">
+                            <div class="flex items-center">
+                                <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
+                            </div>
+                        </td>
+                        <th scope="row" class="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="ps-3">
+                                <div class="text-base font-semibold">Jese Leos</div>
+                                <div class="font-normal text-gray-500">jese@flowbite.com</div>
+                            </div>
+                        </th>
+                        <td class="px-6 py-4">
+                            Standard
+                        </td>
+                        <td class="px-6 py-4">
+                        <div class="flex flex-col gap-1">
+                                <p>Revisar cotización</p>
+                                <p>Acceso al Home</p>
+                                <p>Acceso al Dashboard</p>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar usuario</a>
+                        </td>
+                    </tr>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="w-4 p-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
+                            </div>
+                        </td>
+                        <th scope="row" class="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="ps-3">
+                                <div class="text-base font-semibold">Thomas Lean</div>
+                                <div class="font-normal text-gray-500">thomes@flowbite.com</div>
+                            </div>
+                        </th>
+                        <td class="px-6 py-4">
+                            Standard
+                        </td>
+                        <td class="px-6 py-4">
+                          <div class="flex flex-col gap-1">
+                                <p>Revisar cotización</p>
+                                <p>Acceso al Home</p>
+                                <p>Acceso al Dashboard</p>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar usuario</a>
+                        </td>
+                    </tr>
+                    <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="w-4 p-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-table-search-3" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                                <label for="checkbox-table-search-3" class="sr-only">checkbox</label>
+                            </div>
+                        </td>
+                        <th scope="row" class="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="ps-3">
+                                <div class="text-base font-semibold">Leslie Livingston</div>
+                                <div class="font-normal text-gray-500">leslie@flowbite.com</div>
+                            </div>
+                        </th>
+                        <td class="px-6 py-4">
+                            End User
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex flex-col gap-1">
+                                <p>Solicitar cotización</p>
+                                <p>Acceso al Home</p>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar usuario</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
         </main>
       </div>
       
--- front-pastelero/src/components/asideadmin.jsx
+++ front-pastelero/src/components/asideadmin.jsx
@@ -36,7 +36,7 @@ const Asideadmin = () => {
           <Link href="/dashboard/usuarios">
             <li>
               <Link
-                href="#"
+                href="/dashboard/usuarios"
                 class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
