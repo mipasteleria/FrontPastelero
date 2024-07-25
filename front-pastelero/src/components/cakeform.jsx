6c5a66c730f86b2808275bd2cc7aab9502519c09
Alexis Fuentes <lexisnvm@gmail.com>
2024-07-24 23:52:08 -0500

    detalle del form

--- front-pastelero/src/components/cakeform.jsx
+++ front-pastelero/src/components/cakeform.jsx
@@ -117,140 +117,141 @@ const CakeForm = () => {
   };
 
   return (
-  
     <form onSubmit={handleSubmit} className={`${poppins.className}`}>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 text-sm font-medium dark:text-white">
-      <div className="md:col-span-3">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        
-        <div>
-          <label>Sabor del Bizcocho</label>
-          <select
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-            name="cakeFlavor"
-            value={formData.cakeFlavor}
-            onChange={handleInputChange}
-          >
-            <option value="">Selecciona un sabor</option>
-            {cakeFlavor.map((flavor) => (
-              <option key={flavor} value={flavor}>
-                {flavor}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Número de Porciones</label>
-          <select
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-            name="portions"
-            value={formData.portions}
-            onChange={handleInputChange}
-          >
-            <option value="">Selecciona el número de porciones</option>
-            {[...Array(20)].map((_, i) => (
-              <option key={i} value={(i + 1) * 10}>
-                {(i + 1) * 10}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Número de Niveles</label>
-          <select
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-            name="levels"
-            value={formData.levels}
-            onChange={handleInputChange}
-          >
-            <option value="">Selecciona el número de niveles</option>
-            {[...Array(5)].map((_, i) => (
-              <option key={i} value={i + 1}>
-                {i + 1}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Sabor del Relleno</label>
-          <select
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-            name="cakeFilling"
-            value={formData.cakeFilling}
-            onChange={handleInputChange}
-          >
-            <option value="">Selecciona el sabor del relleno</option>
-            {cakeFilling.map((filling) => (
-              <option key={filling} value={filling}>
-                {filling}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Cobertura</label>
-          <select
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-            name="cober"
-            value={formData.cober}
-            onChange={handleInputChange}
-          >
-            <option value="">Selecciona la cobertura</option>
-            {cober.map((cover) => (
-              <option key={cover} value={cover}>
-                {cover}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <div>
-          <label>Requiere Envio</label>
-          <select
-            id="envio"
-            name="envio"
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-            value={formData.envio}
-            onChange={handleInputChange}
-            required
-          >
-            <option value="">Selecciona una opción</option>
-            <option value="si">Sí</option>
-            <option value="no">No</option>
-          </select>
-        </div>
-        <div>
-          <label>Lugar de entrega</label>
-          <input
-            type="text"
-            id="LugEnt"
-            name="LugEnt"
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-            placeholder={formData.envio === "no" ? "Se recogerá en sucursal" : "Calle, número y colonia"}
-            value={formData.LugEnt}
-            onChange={handleInputChange}
-            disabled={formData.envio === "no"}
-            required
-          />
-        </div>
-        <div class="flex items-center mb-4">
-        <label class="ms-2 text-sm font-medium text-text">
-            <input
-              type="checkbox"
-              name="fondant"
-              checked={formData.fondant}
-              onChange={handleInputChange}
-              class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-accent focus:ring-2 focus:border-accent"
-            />
-            Forrado de Fondant
-          </label>
-          </div>
-        </div>
-        </div>
-            <div className="md:col-span-1 flex items-start">
-            <DatePicker />
+        <div className="md:col-span-3">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <label>Sabor del Bizcocho</label>
+              <select
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                name="cakeFlavor"
+                value={formData.cakeFlavor}
+                onChange={handleInputChange}
+              >
+                <option value="">Selecciona un sabor</option>
+                {cakeFlavor.map((flavor) => (
+                  <option key={flavor} value={flavor}>
+                    {flavor}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Número de Porciones</label>
+              <select
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                name="portions"
+                value={formData.portions}
+                onChange={handleInputChange}
+              >
+                <option value="">Selecciona el número de porciones</option>
+                {[...Array(20)].map((_, i) => (
+                  <option key={i} value={(i + 1) * 10}>
+                    {(i + 1) * 10}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Número de Niveles</label>
+              <select
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                name="levels"
+                value={formData.levels}
+                onChange={handleInputChange}
+              >
+                <option value="">Selecciona el número de niveles</option>
+                {[...Array(5)].map((_, i) => (
+                  <option key={i} value={i + 1}>
+                    {i + 1}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Sabor del Relleno</label>
+              <select
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                name="cakeFilling"
+                value={formData.cakeFilling}
+                onChange={handleInputChange}
+              >
+                <option value="">Selecciona el sabor del relleno</option>
+                {cakeFilling.map((filling) => (
+                  <option key={filling} value={filling}>
+                    {filling}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Cobertura</label>
+              <select
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                name="cober"
+                value={formData.cober}
+                onChange={handleInputChange}
+              >
+                <option value="">Selecciona la cobertura</option>
+                {cober.map((cover) => (
+                  <option key={cover} value={cover}>
+                    {cover}
+                  </option>
+                ))}
+              </select>
             </div>
 
+            <div>
+              <label>Requiere Envio</label>
+              <select
+                id="envio"
+                name="envio"
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                value={formData.envio}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Selecciona una opción</option>
+                <option value="si">Sí</option>
+                <option value="no">No</option>
+              </select>
+            </div>
+            <div>
+              <label>Lugar de entrega</label>
+              <input
+                type="text"
+                id="LugEnt"
+                name="LugEnt"
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                placeholder={
+                  formData.envio === "no"
+                    ? "Se recogerá en sucursal"
+                    : "Calle, número y colonia"
+                }
+                value={formData.LugEnt}
+                onChange={handleInputChange}
+                disabled={formData.envio === "no"}
+                required
+              />
+            </div>
+            <div class="flex items-center mb-4">
+              <label class="ms-2 text-sm font-medium text-text">
+                <input
+                  type="checkbox"
+                  name="fondant"
+                  checked={formData.fondant}
+                  onChange={handleInputChange}
+                  class="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-accent focus:ring-2 focus:border-accent"
+                />
+                Forrado de Fondant
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="md:col-span-1 flex items-start">
+          <DatePicker />
+        </div>
       </div>
       <div className="flex flex-col bg-rose-50 p-6 mb-6 rounded-lg">
         <h2 className={`text-xl m-4 ${sofia.className}`}>
@@ -258,7 +259,10 @@ const CakeForm = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {cakeDecoreOptions.map((option) => (
-            <label key={option} className="ms-2 text-sm font-medium text-text flex items-center">
+            <label
+              key={option}
+              className="ms-2 text-sm font-medium text-text flex items-center"
+            >
               <input
                 type="checkbox"
                 name="decorations"
@@ -282,12 +286,15 @@ const CakeForm = () => {
         </div>
       </div>
       <div className="m-6">
-      <p className="my-2">
-        Por favor, sube imágenes de inspiración, como la temática, los elementos que te gustaría ver en el pastel, la paleta de colores u otras preferencias.
-      </p>
-      <p className="my-2">
-        Esto nos ayudará a crear un diseño personalizado para ti. Puedes subir hasta 5 imágenes de hasta 10MB cada una.
-      </p>
+        <p className="my-2">
+          Por favor, sube imágenes de inspiración, como la temática, los
+          elementos que te gustaría ver en el pastel, la paleta de colores u
+          otras preferencias.
+        </p>
+        <p className="my-2">
+          Esto nos ayudará a crear un diseño personalizado para ti. Puedes subir
+          hasta 5 imágenes de hasta 10MB cada una.
+        </p>
       </div>
       <div className="flex items-center justify-center m-10">
         <label
@@ -311,7 +318,8 @@ const CakeForm = () => {
               />
             </svg>
             <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click to upload</span> or drag and drop
+              <span className="font-semibold">Click to upload</span> or drag and
+              drop
             </p>
             <p className="text-xs text-gray-500 dark:text-gray-400">
               SVG, PNG, JPG or GIF (MAX. 800x400px)
@@ -336,7 +344,9 @@ const CakeForm = () => {
 
       <div className="flex flex-col md:flex-row m-6">
         <p className="md:w-1/2">
-          ¿Podrías informarnos si tienes un presupuesto específico para este pedido? Nos sería de gran ayuda conocer la cantidad que tienes en mente.
+          ¿Podrías informarnos si tienes un presupuesto específico para este
+          pedido? Nos sería de gran ayuda conocer la cantidad que tienes en
+          mente.
         </p>
         <div className="m-4 md:w-1/2">
           <label
@@ -358,68 +368,68 @@ const CakeForm = () => {
         </div>
       </div>
       <div className="m-6">
-      <h2 className={`text-3xl m-4 ${sofia.className}`}>
-        Información de contacto
-      </h2>
-      <div className="flex flex-col bg-rose-50 p-6 mb-6 rounded-lg">
-        <div className="flex flex-col md:flex-row mb-6">
-          <div className="m-4 md:w-1/2">
-            <label
-              htmlFor="userName"
-              className="block mb-2 text-sm font-medium dark:text-white"
-            >
-              Nombre
-            </label>
-            <input
-              type="text"
-              id="userName"
-              name="userName"
-              className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
-              placeholder="Juan de Dios"
-              value={formData.userName}
-              onChange={handleInputChange}
-              required
-            />
+        <h2 className={`text-3xl m-4 ${sofia.className}`}>
+          Información de contacto
+        </h2>
+        <div className="flex flex-col bg-rose-50 p-6 mb-6 rounded-lg">
+          <div className="flex flex-col md:flex-row mb-6">
+            <div className="m-4 md:w-1/2">
+              <label
+                htmlFor="userName"
+                className="block mb-2 text-sm font-medium dark:text-white"
+              >
+                Nombre
+              </label>
+              <input
+                type="text"
+                id="userName"
+                name="userName"
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
+                placeholder="Juan de Dios"
+                value={formData.userName}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="m-4 md:w-1/2">
+              <label
+                htmlFor="userPhone"
+                className="block mb-2 text-sm font-medium dark:text-white"
+              >
+                Número de celular
+              </label>
+              <input
+                type="text"
+                id="userPhone"
+                name="userPhone"
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
+                placeholder="9613202460"
+                value={formData.userPhone}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
           </div>
-          <div className="m-4 md:w-1/2">
+          <div className="m-4">
             <label
-              htmlFor="userPhone"
+              htmlFor="userComments"
               className="block mb-2 text-sm font-medium dark:text-white"
             >
-              Número de celular
+              Preguntas o comentarios
             </label>
-            <input
-              type="text"
-              id="userPhone"
-              name="userPhone"
+            <textarea
+              id="userComments"
+              name="userComments"
               className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
-              placeholder="9613202460"
-              value={formData.userPhone}
+              placeholder=""
+              value={formData.userComments}
               onChange={handleInputChange}
               required
+              rows="6"
+              style={{ resize: "none" }}
             />
           </div>
         </div>
-        <div className="m-4">
-          <label
-            htmlFor="userComments"
-            className="block mb-2 text-sm font-medium dark:text-white"
-          >
-            Preguntas o comentarios
-          </label>
-          <textarea
-            id="userComments"
-            name="userComments"
-            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
-            placeholder=""
-            value={formData.userComments}
-            onChange={handleInputChange}
-            required
-            rows="6"
-            style={{ resize: "none" }}
-          />
-        </div>
-      </div>
       </div>
 
       {/* Botón para limpiar campos */}
@@ -432,13 +442,12 @@ const CakeForm = () => {
           Limpiar campos
         </button>
         <button
-        type="submit"
-        className="bg-secondary text-white py-2 px-4 rounded hover:bg-accent transition"
-      >
-        Cotizar Pastel
-      </button>
+          type="submit"
+          className="bg-secondary text-white py-2 px-4 rounded hover:bg-accent transition"
+        >
+          Cotizar Pastel
+        </button>
       </div>
-      
     </form>
   );
 };
