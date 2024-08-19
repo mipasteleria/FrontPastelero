e3da2cfe6a2b854ecf1732523f04aa41d479a25b
albores339 <juandediosalboreshernandez@gmail.com>
2024-08-19 01:25:07 -0600

    intento de post

--- front-pastelero/pages/enduser/solicitarcotizacion.jsx
+++ front-pastelero/pages/enduser/solicitarcotizacion.jsx
@@ -4,60 +4,42 @@ import NavbarAdmin from "@/src/components/navbar";
 import CakeForm from "@/src/components/cakeform";
 import CupcakeForm from "@/src/components/cupcakeform";
 import DessertTableForm from "@/src/components/dessertsform";
-import ContactInfo from "@/src/components/contactinfo";
 import { Poppins as PoppinsFont, Sofia as SofiaFont } from "next/font/google";
-import { useForm } from "react-hook-form";
 
 const poppins = PoppinsFont({ subsets: ["latin"], weight: ["400", "700"] });
 const sofia = SofiaFont({ subsets: ["latin"], weight: ["400"] });
 
 export default function SolicitarCotizacion() {
-  const { register, handleSubmit, formState: { errors }, watch } = useForm();
   const [selectedForm, setSelectedForm] = useState("cake");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevFormData => ({ ...prevFormData, [name]: value }));
+  };
 
   const handleFormSelection = (e) => setSelectedForm(e.target.value);
 
-  const onSubmit = async (data) => {
-    try {
-      const response = await fetch('http:localhost:3001/pricecake', {  // Reemplaza '/api/submitForm' con la URL de tu API
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-  
-      if (!response.ok) {
-        throw new Error('Error al enviar los datos');
-      }
-  
-      const result = await response.json();
-      console.log('Form submission successful:', result);
-    } catch (error) {
-      console.error('Error submitting form:', error);
-    }
-  };  
-
-  const getButtonText = () => {
-    if (selectedForm === "cake") return "Cotizar pastel";
-    if (selectedForm === "cupcake") return "Cotizar cupcakes";
-    if (selectedForm === "dessertTable") return "Cotizar mesa de postres";
-    return "Cotizar"; // 
-  };
-  
   return (
     <div>
       <NavbarAdmin />
       <main className={`text-text ${poppins.className} mt-24 max-w-screen-lg mx-auto`}>
         <h1 className={`text-4xl m-4 ${sofia.className}`}>Solicitar cotización</h1>
         <p className="m-6">
-          Le pedimos que complete cada campo con la mayor cantidad de detalles posible para acelerar el proceso de cotización. Recuerda que somos una empresa pequeña que realiza pocos pasteles a la semana. Por favor, solicita tu cotización con suficiente anticipación. Hacemos todo lo posible para responder rápidamente, pero a veces puede haber retrasos. Agradecemos tu comprensión.
+          Le pedimos que complete cada campo con la mayor cantidad de detalles
+          posible para acelerar el proceso de cotización. Recuerda que somos es
+          una empresa pequeña que realiza pocos pasteles a la semana. Por favor,
+          solicita tu cotización con suficiente anticipación. Hacemos todo lo
+          posible para responder rápidamente, pero a veces puede haber retrasos.
+          Agradecemos tu comprensión.
         </p>
-        <form className="m-4" onSubmit={handleSubmit(onSubmit)}>
+
+        <form className="m-4" onSubmit={(e) => e.preventDefault()}>
           <div className="mb-6">
-            <label className={`text-2xl m-4 ${sofia.className}`}>Selecciona el producto que deseas cotizar:</label>
-            <div className="flex justify-between mt-6">
-              {["cake", "cupcake", "dessertTable"].map((type) => (
+            <label className={`text-2xl m-4 ${sofia.className}`}>
+              Selecciona el producto que deseas cotizar:
+            </label>
+            <div className="flex justify-between">
+              {["cake", "cupcake", "dessertTable"].map(type => (
                 <label key={type} className="mr-4">
                   <input
                     type="radio"
@@ -71,21 +53,11 @@ export default function SolicitarCotizacion() {
                 </label>
               ))}
             </div>
-            {errors.formType && <p className="text-red-500">{errors.formType.message}</p>}
           </div>
 
-          {selectedForm === "cake" && <CakeForm register={register} errors={errors} watch={watch} />}
-          {selectedForm === "cupcake" && <CupcakeForm register={register} errors={errors} watch={watch} />}
-          {selectedForm === "dessertTable" && <DessertTableForm register={register} errors={errors} watch={watch} />}
-
-          <ContactInfo register={register} errors={errors} />
-
-          <button
-            type="submit"
-            className="text-white bg-secondary hover:bg-accent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-6"
-          >
-            {getButtonText()}
-          </button>
+          {selectedForm === "cake" && <CakeForm className="w-3/4 p-4" />}
+          {selectedForm === "cupcake" && <CupcakeForm className="w-3/4 p-4" />}
+          {selectedForm === "dessertTable" && <DessertTableForm className="w-3/4 p-4" />}
         </form>
       </main>
       <WebFooter />
--- front-pastelero/src/components/cakeform.jsx
+++ front-pastelero/src/components/cakeform.jsx
@@ -1,77 +1,145 @@
+import { useState } from "react";
 import { Poppins as PoppinsFont, Sofia as SofiaFont } from "next/font/google";
+import Link from "next/link";
 import DatePicker from "@/src/components/calendario";
+
 const poppins = PoppinsFont({ subsets: ["latin"], weight: ["400", "700"] });
 const sofia = SofiaFont({ subsets: ["latin"], weight: ["400"] });
 
-const CakeForm = ({ register, errors, watch, setValue }) => {
-  const cakeFlavor = ["Bizcocho de Vainilla", "Bizcocho de Chocolate", "Bizcocho Red Velvet", "Bizcocho de Naranja", "Bizcocho Dulce de Leche"];
-  const cakeFilling = ["Buttercream Vainilla", "Buttercream Chocolate", "Ganache Chocolate semiamargo", "Ganache Chocolate Blanco", "Mermelada de blueberry y LemonCurd", "Queso Crema", "Mermelada Frambuesa", "Mermelada Maracuya"];
-  const cober = ["Buttercream Vainilla", "Buttercream Chocolate", "Ganache Chocolate semiamargo", "Ganache Chocolate Blanco"];
-  const cakeDecoreOptions = ["Cobertura Buttercream(betun con base en mantequilla)", "Cobertura Ganache Base de Chocolate", "Forrado de Fondant", "Dibujo a mano en Fondant", "Dibujo en Buttercream", "Flores naturales", "Letrero", "Impresion comestible", "Personajes modelados de azúcar", "Pastel 3d de un personaje"];
-  const selectedDecorations = watch("decorations", []); 
-  const envioValue = watch("envio");
+const CakeForm = () => {
+  const cakeFlavor = [
+    "Bizcocho de Vainilla", "Bizcocho de Chocolate", "Bizcocho Red Velvet", 
+    "Bizcocho de Naranja", "Bizcocho Dulce de Leche"
+  ];
+  const cakeFilling = [
+    "Buttercream Vainilla", "Buttercream Chocolate", "Ganache Chocolate semiamargo", 
+    "Ganache Chocolate Blanco", "Mermelada de blueberry y LemonCurd", 
+    "Queso Crema", "Mermelada Frambuesa", "Mermelada Maracuya"
+  ];
+  const cober = [
+    "Buttercream Vainilla", "Buttercream Chocolate", "Ganache Chocolate semiamargo", 
+    "Ganache Chocolate Blanco"
+  ];
+  const cakeDecoreOptions = [
+    "Cobertura Buttercream(betun con base en mantequilla)", 
+    "Cobertura Ganache Base de Chocolate", "Forrado de Fondant", 
+    "Dibujo a mano en Fondant", "Dibujo en Buttercream", "Flores naturales", 
+    "Letrero", "Impresion comestible", "Personajes modelados de azúcar", 
+    "Pastel 3d de un personaje"
+  ];
+
+  const [formData, setFormData] = useState({
+    cakeFlavor: "", portions: "", levels: "", cakeFilling: "", cober: "", 
+    fondant: false, decorations: [], others: "", envio: "", LugEnt: "", 
+    budget: "", images: [], userName: "", userPhone: "", userComments: "", 
+    Date: "", Hour: ""
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value
+    }));
+  };
 
   const handleDecorationsChange = (decoration) => {
-    const updatedDecorations = selectedDecorations.includes(decoration)
-      ? selectedDecorations.filter((item) => item !== decoration)
-      : [...selectedDecorations, decoration];
-    setValue("decorations", updatedDecorations);
+    setFormData((prevData) => {
+      const decorations = prevData.decorations.includes(decoration)
+        ? prevData.decorations.filter((item) => item !== decoration)
+        : [...prevData.decorations, decoration];
+      return { ...prevData, decorations };
+    });
+  };
+
+  const handleClearFields = () => {
+    setFormData({
+      cakeFlavor: "", portions: "", levels: "", cakeFilling: "", cober: "", 
+      fondant: false, decorations: [], others: "", envio: "", LugEnt: "", 
+      budget: "", images: [], userName: "", userPhone: "", Date: "", 
+      Hour: "", userComments: ""
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
   };
 
   return (
-    <form className={`${poppins.className}`}>
+    <form onSubmit={handleSubmit} className={`${poppins.className}`}>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-4 text-sm font-medium dark:text-white">
         <div className="md:col-span-3">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {[
-              { label: "Sabor del Bizcocho", name: "flavor", options: cakeFlavor },
+              { label: "Sabor del Bizcocho", name: "cakeFlavor", options: cakeFlavor },
               { label: "Número de Porciones", name: "portions", options: [...Array(20)].map((_, i) => (i + 1) * 10) },
               { label: "Número de Niveles", name: "levels", options: [...Array(5)].map((_, i) => i + 1) },
-              { label: "Sabor del Relleno", name: "stuffedFlavor", options: cakeFilling },
-              { label: "Cobertura", name: "cober", options: cober },
-              { label: "Requiere Envío", name: "envio", options: ["si", "no"] },
+              { label: "Sabor del Relleno", name: "cakeFilling", options: cakeFilling },
+              { label: "Cobertura", name: "cober", options: cober }
             ].map(({ label, name, options }) => (
               <div key={name}>
                 <label>{label}</label>
                 <select
                   className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-                  {...register(name, { required: "Este campo es obligatorio" })}
+                  name={name}
+                  value={formData[name]}
+                  onChange={handleInputChange}
                 >
-                  <option value="">{`Selecciona ${label.toLowerCase()}`}</option>
+                  <option value="">Selecciona una opción</option>
                   {options.map((option) => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
+                    <option key={option} value={option}>{option}</option>
                   ))}
                 </select>
-                {errors[name] && <p className="text-red-500">{errors[name].message}</p>}
               </div>
             ))}
+            <div>
+              <label>Requiere Envio</label>
+              <select
+                id="envio"
+                name="envio"
+                className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+                value={formData.envio}
+                onChange={handleInputChange}
+                required
+              >
+                <option value="">Selecciona una opción</option>
+                <option value="si">Sí</option>
+                <option value="no">No</option>
+              </select>
+            </div>
             <div>
               <label>Lugar de entrega</label>
               <input
                 type="text"
+                id="LugEnt"
+                name="LugEnt"
                 className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
-                placeholder="Calle, número y colonia"
-                {...register("deliveryAddress", { required: envioValue !== "no" })}
+                placeholder={formData.envio === "no" ? "Se recogerá en sucursal" : "Calle, número y colonia"}
+                value={formData.LugEnt}
+                onChange={handleInputChange}
+                disabled={formData.envio === "no"}
+                required
               />
-              {errors.deliveryAddress && <p className="text-red-500">{errors.deliveryAddress.message}</p>}
             </div>
           </div>
         </div>
         <div className="md:col-span-1 flex items-start">
-          <DatePicker {...register("deliveryDate")} />
+          <DatePicker />
         </div>
       </div>
       <div className="flex flex-col bg-rose-50 p-6 mb-6 rounded-lg">
-        <h2 className={`text-xl m-4 ${sofia.className}`}>Elige las opciones de decoración que te gustaría que tenga tu pastel</h2>
+        <h2 className={`text-xl m-4 ${sofia.className}`}>
+          Elige las opciones de decoración que te gustaría que tenga tu pastel
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {cakeDecoreOptions.map((option) => (
             <label key={option} className="ms-2 text-sm font-medium text-text flex items-center">
               <input
                 type="checkbox"
+                name="decorations"
                 className="w-4 h-4 text-secondary bg-gray-100 border-gray-300 rounded focus:ring-accent focus:ring-2 focus:border-accent"
-                checked={selectedDecorations.includes(option)}
+                checked={formData.decorations.includes(option)}
                 onChange={() => handleDecorationsChange(option)}
               />
               <span className="ml-2">{option}</span>
@@ -83,11 +151,121 @@ const CakeForm = ({ register, errors, watch, setValue }) => {
           <input
             type="text"
             className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 mt-2"
-            {...register("other")}
-            placeholder="Indica otro tipo de decoración que te gustaría"
+            name="others"
+            value={formData.others}
+            onChange={handleInputChange}
           />
         </div>
       </div>
+      <div className="m-6">
+  <p className="my-2">Por favor, sube imágenes de inspiración, como la temática, los elementos que te gustaría ver en el pastel, la paleta de colores u otras preferencias.</p>
+  <p className="my-2">Esto nos ayudará a crear un diseño personalizado para ti. Puedes subir hasta 5 imágenes de hasta 10MB cada una.</p>
+  </div>
+  <div className="flex items-center justify-center m-10">
+    <label
+      htmlFor="dropzone-file"
+      className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100"
+    >
+      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+        </svg>
+        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+      </div>
+      <input
+        id="dropzone-file"
+        type="file"
+        className="hidden"
+        multiple
+        accept=".png, .jpg, .jpeg"
+        onChange={handleInputChange}
+      />
+    </label>
+  </div>
+  <div className="bg-gray-200 rounded-full m-10">
+    <div className="bg-primary text-xs font-medium text-text text-center p-0.5 leading-none rounded-full">45%</div>
+  </div>
+  <div className="flex flex-col md:flex-row m-6">
+    <p className="md:w-1/2">¿Podrías informarnos si tienes un presupuesto específico para este pedido? Nos sería de gran ayuda conocer la cantidad que tienes en mente.</p>
+    <div className="m-4 md:w-1/2">
+      <label htmlFor="budget" className="block mb-2 text-sm font-medium dark:text-white">Presupuesto deseado</label>
+      <input
+        type="number"
+        id="budget"
+        name="budget"
+        className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5"
+        placeholder="0.0"
+        value={formData.budget}
+        onChange={handleInputChange}
+        required
+      />
+    </div>
+  </div>
+  <div className="m-6">
+    <h2 className={`text-3xl m-4 ${sofia.className}`}>Información de contacto</h2>
+    <div className="flex flex-col bg-rose-50 p-6 mb-6 rounded-lg">
+      <div className="flex flex-col md:flex-row mb-6">
+        <div className="m-4 md:w-1/2">
+          <label htmlFor="userName" className="block mb-2 text-sm font-medium dark:text-white">Nombre</label>
+          <input
+            type="text"
+            id="userName"
+            name="userName"
+            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
+            placeholder="Juan de Dios"
+            value={formData.userName}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="m-4 md:w-1/2">
+          <label htmlFor="userPhone" className="block mb-2 text-sm font-medium dark:text-white">Número de celular</label>
+          <input
+            type="text"
+            id="userPhone"
+            name="userPhone"
+            className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
+            placeholder="9613202460"
+            value={formData.userPhone}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+      </div>
+      <div className="m-4">
+        <label htmlFor="userComments" className="block mb-2 text-sm font-medium dark:text-white">Preguntas o comentarios</label>
+        <textarea
+          id="userComments"
+          name="userComments"
+          className="bg-gray-50 border border-secondary text-sm rounded-lg focus:ring-accent focus:border-accent block w-full p-2.5 dark:placeholder-secondary dark:focus:border-accent"
+          placeholder=""
+          value={formData.userComments}
+          onChange={handleInputChange}
+          required
+          rows="6"
+          style={{ resize: "none" }}
+        />
+      </div>
+    </div>
+  </div>
+  <div className="flex flex-col md:flex-row items-center justify-end m-4 mb-8 gap-4 ml-4">
+    <button
+      type="button"
+      className="bg-secondary text-white py-2 px-4 rounded hover:bg-accent transition"
+      onClick={handleClearFields}
+    >
+      Limpiar campos
+    </button>
+    <Link href="/enduser/detallecotizacionpastel">
+      <button
+        type="submit"
+        className="bg-secondary text-white py-2 px-4 rounded hover:bg-accent transition"
+      >
+        Cotizar Pastel
+      </button>
+    </Link>
+  </div>
     </form>
   );
 };
