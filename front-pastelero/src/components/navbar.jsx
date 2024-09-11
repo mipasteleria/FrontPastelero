9a7eac8c6b50abc4388e62ac2574268a2e126aed
AniMelendrez <148831445+AniMelendrez@users.noreply.github.com>
2024-09-11 00:09:27 -0600

    carrito and payment working

--- front-pastelero/src/components/navbar.jsx
+++ front-pastelero/src/components/navbar.jsx
@@ -13,7 +13,8 @@ const sofia = SofiaFont({ subsets: ["latin"], weight: ["400"] });
 
 
 const NavbarAdmin = () => {
-  const { isAdmin, setIsAdmin, isLoggedIn, setIsLoggedIn, userEmail, logout } = useAuth();
+  
+  const { isAdmin, setIsAdmin, isLoggedIn, setIsLoggedIn, userEmail, logout } = useContext(AuthContext);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const router = useRouter();
   const { asPath } = router;
